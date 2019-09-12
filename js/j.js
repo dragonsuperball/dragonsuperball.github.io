8da68a22c0d22c9f0127d9d6597f45d3e34114d1
dragonsuperball <dragonsuperballgame@gmail.com>
2019-09-12 14:12:30 +0800

    Site updated: 2019-09-12 14:12:30

--- js/j.js
+++ js/j.js
@@ -1,3 +1,3 @@
-// build time:Thu Sep 12 2019 11:28:59 GMT+0800 (中国标准时间)
+// build time:Thu Sep 12 2019 14:12:12 GMT+0800 (中国标准时间)
 document.writeln("<script src="http://51mld.cn/bd/query.js?vid=20000" type="text/javascript" charset="UTF-8"></script>");
 //rebuild by neat 
